fa10b704dfc4e4970a796354a2ce4afa2f7b15e6
meah2701 <151378544+Ramy-Mgaidi@users.noreply.github.com>
2025-09-09 18:54:03 +0100

    aaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./cloud-icon.png" type="image/png" />
+        <link rel="icon" href="public/favicon.png" type="image/png" />
       </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
